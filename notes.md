ffd6a4c9e47b6bb77ff92d51bb39957288f5d3f4
Shakibul Islam <8080.shakib@gmail.com>
2023-06-04 09:28:35 +0600

    v 2.0 initial realise

--- notes.md
+++ notes.md
@@ -1,7 +1,2 @@
-# Algorithm.
-
-* Create a book constractor to create new book.
-* Store all new book in myLibrary array.
-* Create a table to display all book
 
 
--- script.js
+++ script.js
@@ -1,112 +1,126 @@
-function main() {
-	// Form display functionality
-	formShowBtn.addEventListener('click', () => {
-		formContainer.classList.remove('d-none')
-	})
+// library array
+const myLibrary = []
 
-	formCloseBtn.addEventListener('click', () => {
-		formContainer.classList.add('d-none')
-	})
-
-	form.addEventListener('submit', (e) => {
-		e.preventDefault()
-		formContainer.classList.add('d-none')
-		inputValidation()
-	})
+// book class
+function Book(name, author, pages, readStatus) {
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.readStatus = readStatus
 }
 
-// Book Constructor
-function Book(title, author, readStatus, pages) {
-	this.title = title
-	this.author = author
-	this.readStatus = readStatus
-	this.pages = pages
+Book.prototype.updateReadStatus = function () {
+  this.readStatus = !this.readStatus
 }
 
-Book.prototype.updateReadStatus = function (status) {
-	this.readStatus = status
+// AddBook function
+function addBookToLibrary(name, author, pages, readStatus) {
+  const book = new Book(name, author, pages, readStatus)
+  myLibrary.push(book)
 }
 
-let myLibrary = []
-
-// DOM Selectors
-const formContainer = document.getElementById('form-container')
-const form = document.getElementById('book-form')
-const formShowBtn = document.getElementById('form-show-btn')
-const formCloseBtn = document.getElementById('form-close-btn')
-const formSubmitBtn = document.getElementById('form-submit-btn')
-const tableBody = document.getElementById('table-body')
-
-function inputValidation() {
-	let bookName = document.getElementById('book-name').value
-	let author = document.getElementById('book-author').value
-	let readStatus = document.getElementById('read-status').checked
-	let pages = document.getElementById('book-pages').value
-
-	if (bookName === '' || author === '' || pages === '') {
-		console.log('false')
-		return false
-	} else {
-		addBookToLibrary(bookName, author, readStatus, pages)
-	}
+// RemoveBook function
+function removeBookFromLibrary(index) {
+  // Based on index remove the book from library array
+  myLibrary.forEach((_, i) => {
+    if (i == index) {
+      myLibrary.splice(i, 1)
+    }
+  })
 }
 
-function addBookToLibrary(bookName, author, readStatus, pages) {
-	let book = new Book(bookName, author, pages, readStatus)
-	myLibrary.push(book)
-	displayBooks()
+// updateReadStatus function
+function updateReadStatus(index) {
+  myLibrary.forEach((book, i) => {
+    if (i == index) {
+      book.updateReadStatus()
+    }
+  })
 }
 
-function displayBooks() {
-	if (myLibrary.length) {
-		const dataRows = document.querySelectorAll('.data-row')
-		dataRows.forEach((row) => {
-			row.remove()
-		})
-	}
-
-	myLibrary.forEach((book, index) => {
-		let row = document.createElement('tr')
-		row.classList.add('data-row')
-		row.innerHTML = `<td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.pages}</td>
-    <td class="text-center"><button class="btn ${book.readStatus? "btn-success" : "btn-warning"} read-btn" data-index="${index}">${
-			book.readStatus ? 'Read' : 'Not Read'
-		}</button></td>
-    <td class="text-center"><button class="btn btn-danger delete-btn" data-index="${index}">Delete</button></td>`
-		tableBody.appendChild(row)
-	})
-
-	deleteElement()
-	updateStatus()
+// form validation function
+function inputValidation(name, author, pages) {
+  if (name == '' || author == '' || pages == '') {
+    return false
+  }
+  return true
 }
 
-function deleteElement() {
-	const deleteButtons = document.querySelectorAll('.delete-btn')
-	deleteButtons.forEach((button) => {
-		button.addEventListener('click', () => {
-			button.parentElement.parentElement.remove()
-			myLibrary.splice(button.getAttribute('data-index'), 1)
-			displayBooks()
-		})
-	})
+// Render function
+function render() {
+  // Delete everything from UI
+  const tableBody = document.getElementById('table-body')
+  tableBody.innerHTML = ''
+
+  // Render everything from myLibrary to UI
+  myLibrary.forEach((book, index) => {
+    let tableRow = document.createElement('tr')
+
+    tableRow.innerHTML = `
+		<td>${book.name}</td>
+		<td>${book.author}</td>
+		<td class="text-center">${book.pages}</td>
+		<td class="text-center">
+			<a href="#" data-index="${index}" class="btn read-status ${
+      book.readStatus ? 'btn--success' : 'btn--danger'
+    }">${book.readStatus ? 'Already read' : 'Not read yet'}</a>
+		</td>
+		<td class="text-center">
+			<a href="#" data-index="${index}" class="btn btn--danger delete">X</a>
+		</td>
+		`
+    tableBody.appendChild(tableRow)
+  })
 }
 
-function updateStatus() {
-	const readButton = document.querySelectorAll('.read-btn')
-	readButton.forEach((btn) => {
-		btn.addEventListener('click', () => {
-			let index = btn.getAttribute('data-index')
-			if (myLibrary[index].readStatus) {
-				myLibrary[index].updateReadStatus(false)
-				displayBooks()
-			} else {
-				myLibrary[index].updateReadStatus(true)
-				displayBooks()
-			}
-		})
-	})
+// Alert function
+function showAlert(msg, cls) {
+  const container = document.querySelector('#container')
+  const table = document.querySelector('#table')
+  const alert = document.createElement('div')
+  alert.innerHTML = msg
+  alert.className = `alert ${cls}`
+
+  container.insertBefore(alert, table)
+  window.setTimeout(() => {
+    alert.remove()
+  }, 3000)
 }
 
-main()
+// event: on document load render all books first time
+document.addEventListener('DOMContentLoaded', () => {
+  render()
+})
+
+// Event: on submitting form button add the books
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const name = document.getElementById('name')
+  const author = document.getElementById('author')
+  const pages = document.getElementById('pages')
+  const readStatus = document.getElementById('read-status')
+
+  if (inputValidation(name.value, author.value, pages.value)) {
+    addBookToLibrary(name.value, author.value, pages.value, readStatus.checked)
+    render()
+    showAlert('Book added successfully!', 'alert__success')
+  } else {
+    showAlert('Please fill up the form!', 'alert__danger')
+  }
+})
+
+// Event: On click on delete button remove books and o click on readStatus button update read status
+document.getElementById('table-body').addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    removeBookFromLibrary(Number(e.target.getAttribute('data-index')))
+    render()
+    showAlert('Book deleted!!', 'alert__success')
+  }
+
+  if (e.target.classList.contains('read-status')) {
+    updateReadStatus(Number(e.target.getAttribute('data-index')))
+    render()
+    showAlert('Read status updated!!', 'alert__success')
+  }
+})
